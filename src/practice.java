52acf3835d75ae667a385884c68ebce2d22802d9
jinsy <Jinsyz95@gmail.com>
2022-08-16 14:11:35 +0900

    start

---
+++ src/practice.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class practice {
+    public static void main(String... args) {
+        int i=0;
+        while(i<=10) {
+            int j = 0;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
---
+++ src/practicestart.java
@@ -0,0 +1,5 @@
+public class practicestart {
+    public static void main(String[] args) {
+        System.out.println("d");
+    }
+}
