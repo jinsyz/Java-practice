d8ac598a38d3cc2011eb9c7ca0c463f18cd27f4b
jinsy <Jinsyz95@gmail.com>
2022-08-23 20:08:54 +0900

    chapter4

---
+++ src/chapter4/chapter4to7.java
@@ -0,0 +1,9 @@
+package chapter4;
+
+public class chapter4to7 {
+    public static void main(String[] args) {
+        int value = (int)(Math.random()*6)+1;
+        System.out.println("value:"+value);
+
+    }
+}
--- src/practicestart.java
+++
@@ -1,5 +0,0 @@
-public class practicestart {
-    public static void main(String[] args) {
-        System.out.println("d");
-    }
-}
